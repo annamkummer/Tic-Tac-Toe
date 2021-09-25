bde80f0429e0840e5f7cfe6c850abcccc52a59c6
Anna Kummer <annamkummer@gmail.com>
2021-09-25 11:38:34 -0600

    Add turn-taking function to game, link game and player to main and html

--- game.js
+++ game.js
@@ -2,27 +2,36 @@ class Game {
   constructor() {
     this.board = ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'];
     this.players = [];
+    this.turn = null;
   }
 
   createPlayers() {
     var playerSnow = new Player('snow', 'snowflake');
     var playerSun = new Player('sun', 'sunshine');
     this.players.push(playerSnow, playerSun);
+    this.turn = this.players[0];
   }
 
   playToken(box, token) {
     if (this.board[box] === 'empty') {
       this.board.splice(box, 1, token)
     }
+    this.switchTurn();
+  }
+
+  switchTurn() {
+    if (this.turn === this.players[0]) {
+      this.turn = this.players[1];
+    } else {
+      this.turn = this.players[0];
+    }
   }
 }
 
-
-// A game.js file that contains a Game class.
 // A Game should include:
 // *Two Player instances
 // *A way to keep track of the data for the game board
-// A way to keep track of which player’s turn it currently is
+// *A way to keep track of which player’s turn it currently is
 // A way to check the Game’s board data for win conditions
 // A way to detect when a game is a draw (no one has won)
 // A way to reset the Game’s board to begin a new game
