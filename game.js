218fac0410edd9cf280acca2852b6033287dbcc8
Anna Kummer <annamkummer@gmail.com>
2021-09-25 13:39:25 -0600

    Link gameboard to user click

--- game.js
+++ game.js
@@ -22,42 +22,42 @@ class Game {
 
   switchTurn() {
     if (this.turn === this.players[0]) {
-      this.turn = this.players[1];
-    } else {
-      this.turn = this.players[0];
-    }
+      this.turn = this.players[1];
+    } else {
+      this.turn = this.players[0];
+    }
   }
 
   checkForWin() {
-    var winningBoards = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (var i = 0; i < winningBoards.length; i++) {
-      var a = winningBoards[i][0];
-      var b = winningBoards[i][1];
-      var c = winningBoards[i][2];
+    var winningBoards = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (var i = 0; i < winningBoards.length; i++) {
+      var a = winningBoards[i][0];
+      var b = winningBoards[i][1];
+      var c = winningBoards[i][2];
 // Can this conditional be clearer?
-      if ((!(this.board[a] ==='empty') || !(this.board[b] === 'empty') || !(this.board[c] === 'empty')) && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
+      if ((!(this.board[a] ==='empty') || !(this.board[b] === 'empty') || !(this.board[c] === 'empty')) && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
 // Console.logs in 49, 53, 57 to be replaced with DOM interactions.
-        console.log('winner: ', this.board[a])
+        console.log('winner: ', this.board[a])
         return;
-      }
-    }
-    if (!this.board.includes('empty')) {
-      console.log('draw')
+      }
+    }
+    if (!this.board.includes('empty')) {
+      console.log('draw')
       return;
-    }
-    console.log('false')
-  }
+    }
+    console.log('false')
+  }
 
   resetBoard() {
-    this.board = ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'];
-  }
+    this.board = ['empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty', 'empty'];
+  }
 }
--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 var game = new Game();
+
+var box0 = document.querySelector('#box0');
+var box1 = document.querySelector('#box1');
+var box2 = document.querySelector('#box2');
+var box3 = document.querySelector('#box3');
+var box4 = document.querySelector('#box4');
+var box5 = document.querySelector('#box5');
+var box6 = document.querySelector('#box6');
+var box7 = document.querySelector('#box7');
+var box8 = document.querySelector('#box8');
+var gameboard = document.querySelector('#gameboard')
+
+var gameBoxes = [
+  box0,
+  box1,
+  box2,
+  box3,
+  box4,
+  box5,
+  box6,
+  box7,
+  box8
+];
+
+gameboard.addEventListener('click', findBox)
+
+function findBox() {
+  for (var i = 0; i < gameBoxes.length; i++) {
+    if (event.target === gameBoxes[i]) {
+      console.log(event.target)
+    }
+  }
+}
