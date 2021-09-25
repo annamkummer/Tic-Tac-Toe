f93a02b2c7efc8de9d8a8ecae4c23a8ad7d788e3
Anna Kummer <annamkummer@gmail.com>
2021-09-25 11:51:52 -0600

    Add function to check for win or draw

--- game.js
+++ game.js
@@ -26,12 +26,40 @@ class Game {
       this.turn = this.players[0];
     }
   }
+
+  checkForWin() {
+    var winningBoards = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    for (var i = 0; i < winningBoards.length; i++) {
+      var a = winningBoards[i][0];
+      var b = winningBoards[i][1];
+      var c = winningBoards[i][2];
+// Can this conditional be clearer?
+      if ((!(this.board[a] ==='empty') || !(this.board[b] === 'empty') || !(this.board[c] === 'empty')) && (this.board[a] === this.board[b] && this.board[a] === this.board[c])) {
+        console.log('winner: ', this.board[a])
+        return;
+      }
+    }
+    if (!this.board.includes('empty')) {
+      console.log('draw')
+      return;
+    }
+    console.log('false')
+  }
 }
 
 // A Game should include:
 // *Two Player instances
 // *A way to keep track of the data for the game board
 // *A way to keep track of which player’s turn it currently is
-// A way to check the Game’s board data for win conditions
-// A way to detect when a game is a draw (no one has won)
+// *A way to check the Game’s board data for win conditions
+// *A way to detect when a game is a draw (no one has won)
 // A way to reset the Game’s board to begin a new game
