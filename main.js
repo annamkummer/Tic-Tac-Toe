68a938562b19c35660ed086553400795a0a9e655
Anna Kummer <annamkummer@gmail.com>
2021-09-26 15:06:28 -0600

    Refactor html and  end game functionss

--- main.js
+++ main.js
@@ -2,10 +2,7 @@ var game = new Game();
 
 var gameboard = document.querySelector('#gameboard');
 var header = document.querySelector('#header');
-var player0Wins = document.querySelector('#player0Wins');
-var player1Wins = document.querySelector('#player1Wins');
-var snowSideImg = document.querySelector('#snow')
-var sunSideImg = document.querySelector('#sunshine')
+var sidePanels = document.querySelectorAll('.side-panel');
 
 gameboard.addEventListener('click', displayToken);
 
@@ -15,55 +12,49 @@ function newGame() {
   game.createPlayers();
   createGameboard();
   displayTurn();
-  snowSideImg.classList.add('turn');
-  snowSideImg.classList.remove('not-turn');
-  sunSideImg.classList.remove('turn');
-  sunSideImg.classList.add('not-turn');
-  displayWins();
+  emphasizeTurn();
 }
 
 function createGameboard() {
   gameboard.innerHTML =  ``;
   for (var i = 0; i < 9; i++) {
-    gameboard.innerHTML += `<article class='squares box-${i}' id='${i}'></article>`;
+    gameboard.innerHTML += `<article class='free-square box-${i}' id='${i}'></article>`;
   }
 }
 
 function displayTurn() {
   header.innerHTML = `It's <img class='header-image' src=${game.turn.tokenImg} alt=${game.turn.tokenAltText}>'s turn!`;
+  emphasizeTurn();
+}
 
-  if (game.turn.id === 'sunshine') {
-    sunSideImg.classList.add('turn')
-    snowSideImg.classList.remove('turn')
-    sunSideImg.classList.remove('not-turn')
-    snowSideImg.classList.add('not-turn')
-  } else {
-    snowSideImg.classList.add('turn')
-    sunSideImg.classList.remove('turn')
-    snowSideImg.classList.remove('not-turn')
-    sunSideImg.classList.add('not-turn')
+function emphasizeTurn() {
+  for (var i = 0; i < sidePanels.length; i++) {
+    if (game.turn === game.players[i]) {
+      var turn = 'turn';
+    } else {
+      var turn = 'not-turn';
+    }
+    sidePanels[i].innerHTML = `
+      <img class=${turn} src=${game.players[i].tokenImg} alt=${game.players[i].tokenAltText}>
+      <h2>${displayWins(game.players[i])}</h2>
+      `;
   }
 }
 
-function displayWins() {
-  var winDisplayText = [];
-  for (var i = 0; i < game.players.length; i++) {
-    if (game.players[i].wins === 1) {
-      var winText = 'win';
-    } else {
-      var winText = 'wins';
-    }
-    winDisplayText.push(`${game.players[i].wins} ${winText}`);
+function displayWins(player) {
+  if (player.wins === 1) {
+    var winText = 'win';
+  } else {
+    var winText = 'wins';
   }
-  player0Wins.innerText = winDisplayText[0];
-  player1Wins.innerText = winDisplayText[1];
+  return `${player.wins} ${winText}`;
 }
 
 function displayToken() {
-  if (event.target.classList.contains('squares')) {
+  if (event.target.classList.contains('free-square')) {
     game.playToken(event.target.id);
     event.target.innerHTML = `<img class='token-image' src=${game.board[event.target.id].tokenImg} alt=${game.board[event.target.id].tokenAltText}>`;
-    event.target.classList.remove('squares');
+    event.target.classList.remove('free-square');
   }
   checkForWin();
 }
@@ -74,26 +65,24 @@ function checkForWin() {
     displayTurn();
     return;
   } else if (result === 'draw') {
-    header.innerText = `Cat's game!`;
-    showDrawImage();
+    showDrawContent();
   } else {
-    header.innerHTML = `<img class='header-image' src=${result.tokenImg} alt=${result.tokenAltText}> wins!`;
-    showWinImage();
+    showWinContent(result);
   }
   endGame();
 }
 
-function showDrawImage() {
+function showDrawContent() {
+  header.innerText = `Cat's game!`;
   gameboard.innerHTML = `<img class='end-game-image' src='assets/cat.jpeg' alt='cat with tongue out'>`;
 }
 
-function showWinImage() {
+function showWinContent(winner) {
+  header.innerHTML = `<img class='header-image' src=${winner.tokenImg} alt=${winner.tokenAltText}> wins!`;
   gameboard.innerHTML = `<img class='end-game-image' src='assets/lightning.svg' alt='blue lightning bolt'>`;
 }
 
 function endGame() {
   game.resetBoard();
-  setTimeout(createGameboard, 1500);
-  setTimeout(displayTurn, 1500);
-  displayWins();
+  setTimeout(newGame, 1500);
 }
